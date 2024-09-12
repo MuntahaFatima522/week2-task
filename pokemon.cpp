794467ecfb169efd65eaad526e840ab95b4e9a2c
Muntaha Fatima <immuntahafatima@gmain.com>
2024-09-12 20:15:46 +0500

    message

---
+++ pokemon.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    system("color 70");
+cout<<"'._                                        "<<endl;
+cout<<" ' .          ---'                         "<<endl;
+cout<<"  '.'!_...._/'._''                         "<<endl;
+cout<<"    !        /        '                    "<<endl;
+cout<<"    /()   () !      .' '-._                "<<endl;
+cout<<"   |)  .    ()!  /   _.'                   "<<endl;
+cout<<"    |  _'_      .  '.<                     "<<endl;
+cout<<"    .          ''                          "<<endl;
+cout<<"    ' .--    ';|    >|                     "<<endl;
+cout<<"   /      /    |.-'.-'                     "<<endl;
+cout<<"  (_/'   (_/',;|.<'                        "<<endl;
+cout<<"    ! ,        ._'                         "<<endl;
+cout<<"   >  !       /'                           "<<endl;
+cout<<"  (_,-''>   .'                             "<<endl;
+cout<<"    (_.'                                    "<<endl;
+}
